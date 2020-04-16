4dea045777156033d9c3e9bac0ead4aec222f76f
solomon13 <alexeyzherehi@gmail.com>
2020-04-16 11:29:25 +0300

    Samples with WPF

---
+++ TestApp/WPF/WpfSample/WpfApp1/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Test
+{
+    public class Test
+    {
+        public static double WindowWidth = 800;
+    }
+}
---
+++ TestApp/WPF/WpfSample/WpfApp1/TestWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for TestWindow.xaml
+    /// </summary>
+    public partial class TestWindow : Window
+    {
+        public TestWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var fileSelectionDialog = new OpenFileDialog();
+            fileSelectionDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            fileSelectionDialog.Filter = "Bitmap|*.bmp";
+            fileSelectionDialog.Multiselect = false;
+
+            if (fileSelectionDialog.ShowDialog() == true)
+            {
+                PreviewImage.Source = new BitmapImage(new Uri(fileSelectionDialog.FileName));
+                ImageFileText.Text = fileSelectionDialog.FileName;
+            }
+        }
+    }
+}
